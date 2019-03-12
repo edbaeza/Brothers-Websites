67b3a0efc296c2c158401ae21beef8188fa4d46b
edbaeza <baeza.eric37@gmail.com>
2019-03-12 16:13:36 -0400

    Cleaned/changed code for darkmode. Added init script in pages. Theme should load faster now.

--- LuisWeb/scripts.js
+++ LuisWeb/scripts.js
@@ -1,48 +1,27 @@
 var currentTheme = localStorage.getItem("theme");
-console.log(currentTheme);
-
-window.onload = function(){
-    init();
-}
-
+console.log("currentTheme: "+currentTheme);
 
 function init(){
-    // will make sure the theme is correct when user reloads or revisits the page 
-    const body = document.querySelector('body');
-    
+    console.log("Calling init");
+    var body = document.getElementById("mainbody");
     if(currentTheme == "light_theme"){
-        if(body.classList.contains('dark_theme')){
-            document.getElementById("mainbody").setAttribute("class", "light_theme");
-        }
+        body.className = "light_theme";
     }
-    if(currentTheme == "dark_theme"){
-        if(body.classList.contains('light_theme')){
-            document.getElementById("mainbody").setAttribute("class", "dark_theme");
-        }
+    else{
+        body.className = "dark_theme";
     }
-    
 }
 
 function changeTheme(){
     // changes the theme to either light or dark depending on current state
     // save the current theme to localStorage so theme stays persistent
-    const body = document.querySelector('body');
-    
-    if (body.classList.contains('light_theme')) {
-        document.getElementById("mainbody").setAttribute("class", "dark_theme");
+    var body = document.getElementById('mainbody');
+    if(body.className == "light_theme"){
+        body.className = "dark_theme"
         localStorage.setItem("theme", "dark_theme");
     }
-    else {
-        document.getElementById("mainbody").setAttribute("class", "light_theme");
+    else{
+        body.className = "light_theme";
         localStorage.setItem("theme", "light_theme");
     }
-
-    /* Can do: 
-        var element = document.getElementById("mainbody")
-        element.classList.toggle("mystyle")
-
-        without storing the theme in local storage
-        (would go back to light_theme after reloading page)
-    */
-
 }
