6fdc6bc9f1a194ef7760cbf1ec4444637cd9f04a
edbaeza <baeza.eric37@gmail.com>
2019-03-13 02:42:05 -0400

    Button now changes based on current theme

--- LuisWeb/scripts.js
+++ LuisWeb/scripts.js
@@ -4,11 +4,14 @@ console.log("currentTheme: "+currentTheme);
 function init(){
     console.log("Calling init");
     var body = document.getElementById("mainbody");
+    var themeButton = document.getElementById("toggletheme");
     if(currentTheme == "light_theme"){
         body.className = "light_theme";
+        themeButton.innerHTML = "Dark Mode";
     }
     else{
         body.className = "dark_theme";
+        themeButton.innerHTML = "Light Mode";
     }
 }
 
@@ -16,12 +19,15 @@ function changeTheme(){
     // changes the theme to either light or dark depending on current state
     // save the current theme to localStorage so theme stays persistent
     var body = document.getElementById('mainbody');
+    var themeButton = document.getElementById("toggletheme");
     if(body.className == "light_theme"){
         body.className = "dark_theme"
+        themeButton.innerHTML = "Light Mode";
         localStorage.setItem("theme", "dark_theme");
     }
     else{
         body.className = "light_theme";
+        themeButton.innerHTML = "Dark Mode";
         localStorage.setItem("theme", "light_theme");
     }
 }
